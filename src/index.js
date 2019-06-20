462d8d1733259184f873ef96e2ef3116e99a9d83
Jaid <jaid.jsx@gmail.com>
2019-06-21 01:23:39 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 /** @module has-content */
 
+import {isNil, isString, isNumber, isFunction, isObject, isBoolean} from "lodash"
+
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
+ * Returns `true` if the passed value is truthy or has any kind of content
  * @example
  * import hasContent from "has-content"
- * const result = hasContent()
- * result === 1549410770
- * @returns {number} Seconds since epoch
+ * const result = hasContent(" ")
+ * result === false
+ * @example
+ * import hasContent from "has-content"
+ * const result = hasContent("a")
+ * result === true
+ * @returns {boolean} `true` if `value` is truthy or has content
  */
-export default () => Math.floor(Date.now() / 1000)
+export default value => {
+  if (value |> isNil) {
+    return false
+  }
+  if (value |> isFunction) {
+    return true
+  }
+  if (value |> isNumber) {
+    return true
+  }
+  if (value |> isBoolean) {
+    return value === true
+  }
+  if (value |> isString) {
+    return value.trim().length > 0
+  }
+  if (value.length |> isNumber) {
+    return value.length > 0
+  }
+  if (value.length |> isFunction) {
+    return value.length() > 0
+  }
+  if (value |> isObject) {
+    return Object.keys(value).length > 0
+  }
+  return false
+}
--- test/test.js
+++ test/test.js
@@ -4,6 +4,24 @@ const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.jo
 const {default: hasContent} = indexModule
 
 it("should run", () => {
-  const result = hasContent()
-  expect(result).toBeGreaterThan(1549410770)
+  expect(null |> hasContent).toBe(false)
+  expect(undefined |> hasContent).toBe(false)
+  expect("" |> hasContent).toBe(false)
+  expect(" " |> hasContent).toBe(false)
+  expect(0 |> hasContent).toBe(true)
+  expect(1 |> hasContent).toBe(true)
+  expect("a" |> hasContent).toBe(true)
+  expect(Object.create(null) |> hasContent).toBe(false)
+  expect({
+    ...Object.create(null),
+    a: 2,
+  } |> hasContent).toBe(true)
+  expect({} |> hasContent).toBe(false)
+  expect({a: 2} |> hasContent).toBe(true)
+  expect(hasContent |> hasContent).toBe(true)
+  expect(false |> hasContent).toBe(false)
+  expect(true |> hasContent).toBe(true)
+  expect([] |> hasContent).toBe(false)
+  expect([,] |> hasContent).toBe(true) // eslint-disable-line
+  expect([1] |> hasContent).toBe(true)
 })
